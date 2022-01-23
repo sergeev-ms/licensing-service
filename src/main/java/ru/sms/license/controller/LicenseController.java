f924c0f35684cc3e116f18a7f2dbd1abfad1f809
sergeev_ms <sergeevms.33@gmail.com>
2022-01-23 18:20:41 +0300

    Implement hateoas

--- src/main/java/ru/sms/license/controller/LicenseController.java
+++ src/main/java/ru/sms/license/controller/LicenseController.java
@@ -1,6 +1,8 @@
 package ru.sms.license.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -8,6 +10,8 @@ import ru.sms.license.model.License;
 import ru.sms.license.service.LicenseService;
 
 import java.util.Locale;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
 
 @RestController
 @RequestMapping(value = "v1/organization/{organizationId}/license")
@@ -20,6 +24,21 @@ public class LicenseController {
             @PathVariable("organizationId") String organizationId,
             @PathVariable("licenseId") String licenseId) {
         final License license = licenseService.getLicense(licenseId, organizationId);
+
+        license.add(
+                linkTo(methodOn(LicenseController.class)
+                        .getLicense(organizationId, licenseId))
+                        .withSelfRel(),
+                linkTo(methodOn(LicenseController.class)
+                        .updateLicense(organizationId, license, null))
+                        .withRel("updateLicense"),
+                linkTo(methodOn(LicenseController.class)
+                        .createLicense(organizationId, license, null))
+                        .withRel("createLicense"),
+                linkTo(methodOn(LicenseController.class)
+                        .deleteLicense(organizationId, license.getLicenseId(), null))
+                        .withRel("deleteLicense"));
+
         return ResponseEntity.ok(license);
     }
 
