6c13b4cab6e71258b609236d79630fe58943f1cb
sergeev_ms <sergeevms.33@gmail.com>
2022-01-23 18:06:11 +0300

    Add internationalization support

--- src/main/java/ru/sms/license/LicenseServiceApplication.java
+++ src/main/java/ru/sms/license/LicenseServiceApplication.java
@@ -2,6 +2,12 @@ package ru.sms.license;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class LicenseServiceApplication {
@@ -10,4 +16,19 @@ public class LicenseServiceApplication {
         SpringApplication.run(LicenseServiceApplication.class, args);
     }
 
+    @Bean
+    public LocaleResolver localeResolver() {
+        final SessionLocaleResolver sessionLocaleResolver = new SessionLocaleResolver();
+        sessionLocaleResolver.setDefaultLocale(Locale.ENGLISH);
+        return sessionLocaleResolver;
+    }
+
+    @Bean
+    public ResourceBundleMessageSource messageSource() {
+        final ResourceBundleMessageSource resourceBundleMessageSource = new ResourceBundleMessageSource();
+        resourceBundleMessageSource.setUseCodeAsDefaultMessage(true);
+        resourceBundleMessageSource.setBasename("messages");
+        resourceBundleMessageSource.setDefaultEncoding("utf-8");
+        return resourceBundleMessageSource;
+    }
 }
--- src/main/java/ru/sms/license/controller/LicenseController.java
+++ src/main/java/ru/sms/license/controller/LicenseController.java
@@ -1,10 +1,13 @@
 package ru.sms.license.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.sms.license.model.License;
 import ru.sms.license.service.LicenseService;
+
+import java.util.Locale;
 
 @RestController
 @RequestMapping(value = "v1/organization/{organizationId}/license")
@@ -23,21 +26,24 @@ public class LicenseController {
     @PutMapping
     public ResponseEntity<String> updateLicense(
             @PathVariable("organizationId") String organizationId,
-            @RequestBody License request) {
-        return ResponseEntity.ok(licenseService.updateLicense(request, organizationId));
+            @RequestBody License request,
+            @RequestHeader(value = HttpHeaders.ACCEPT_LANGUAGE, required = false) Locale locale) {
+        return ResponseEntity.ok(licenseService.updateLicense(request, organizationId, locale));
     }
 
     @PostMapping
     public ResponseEntity<String> createLicense(
             @PathVariable("organizationId") String organizationId,
-            @RequestBody License request) {
-        return ResponseEntity.ok(licenseService.createLicense(request, organizationId));
+            @RequestBody License request,
+            @RequestHeader(value = HttpHeaders.ACCEPT_LANGUAGE, required = false) Locale locale) {
+        return ResponseEntity.ok(licenseService.createLicense(request, organizationId, locale));
     }
 
     @DeleteMapping("/{licenseId}")
     public ResponseEntity<String> deleteLicense(
             @PathVariable("organizationId") String organizationId,
-            @PathVariable("licenseId") String licenseId) {
-        return ResponseEntity.ok(licenseService.deleteLicense(licenseId, organizationId));
+            @PathVariable("licenseId") String licenseId,
+            @RequestHeader(value = HttpHeaders.ACCEPT_LANGUAGE, required = false) Locale locale) {
+        return ResponseEntity.ok(licenseService.deleteLicense(licenseId, organizationId, locale));
     }
 }
